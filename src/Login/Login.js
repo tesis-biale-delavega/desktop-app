7cfa07212d6a3e69ad152c4a751a31750880cee2
BraianBGit <braian.biale@ing.austral.edu.ar>
2022-10-09 18:23:22 -0300

    login working

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,11 +1,89 @@
-import React from "react";
-import {Box, Container, Link, Stack} from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, Link, Stack, TextField, Typography } from "@mui/material";
+import vegexLogo from "../assets/vegex-logo.png";
+import vegexBackground from "../assets/vegex-background.png";
+import "./Login.scss";
+import { useMutation } from "react-query";
+import * as http from "../utils/http";
+import {useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
 
 const Login = () => {
-    return <Stack m={2}>
-        <Link href={"/signup"}>Register</Link>
-        <Link href={"/projects"}>Projects List</Link>
-    </Stack>
-}
+    const navigate = useNavigate();
+    const [loginData, setLoginData] = useState(undefined);
+
+    const signUpMutation = useMutation((body) => {
+        return http.post(`/spring-api/auth/login`, body);
+    });
+
+    const onSubmit = () => {
+        signUpMutation.mutate(loginData, {
+            onSuccess: (res) => {
+                localStorage.setItem("token", res.token)
+                navigate("/projects")
+            },
+            onError: (error) => {
+                toast.error(error.data.message)
+            }
+        })
+    }
+
+    const areFieldsCompleted = () => {
+        return loginData && loginData?.email && loginData?.password
+    }
+
+    return (
+        <Stack direction={"row"} flexGrow={1} sx={{ overflow: "hidden" }}>
+            <Stack
+                direction={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                flexGrow={1}
+                m={2}
+            >
+                <img src={vegexLogo} alt={"vegex-logo"} />
+                <Typography variant={"h5"} sx={{ color: "#49BB58" }}>
+                    VEGEX
+                </Typography>
+                <Stack mt={4}>
+                    <TextField
+                        id="login-email"
+                        label="Email"
+                        variant="standard"
+                        type={"email"}
+                        fullWidth
+                        margin={"dense"}
+                        onChange={(e) => setLoginData({...loginData, email: e.target.value})}
+                    />
+                    <TextField
+                        id="login-password"
+                        label="Password"
+                        variant="standard"
+                        type={"password"}
+                        fullWidth
+                        margin={"dense"}
+                        sx={{marginTop: 2}}
+                        onChange={(e) => setLoginData({...loginData, password: e.target.value})}
+                    />
+                    <Button variant={"contained"} sx={{ marginTop: 8 }} onClick={onSubmit} disabled={!areFieldsCompleted()}>
+                        Login
+                    </Button>
+                </Stack>
+                <Stack direction={"row"} mt={4} alignItems={"center"}>
+                    <Link href={"/signup"} mr={1} variant={"body1"} underline={"none"}>
+                        Crear una cuenta
+                    </Link>
+                </Stack>
+            </Stack>
+            <Box sx={{ flexGrow: 6 }}>
+                <img
+                    src={vegexBackground}
+                    alt={"vegex-background"}
+                    className={"login-background-img"}
+                />
+            </Box>
+        </Stack>
+    );
+};
 
 export default Login;
